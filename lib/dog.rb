f331010e7fcbf45c32c396ba65ad2f32307567e2
Anysia V <anysiavalko@gmail.com>
2018-11-23 21:18:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,23 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name
     @breed= breed
   end
+  
   
   def name= (name)
     @name = name
   end
   
+  def breed= (breed = "Mutt")
+  
   def name
     @name
   end
+  
+  def breed
+    @breed
+  end
+    
     
 end
